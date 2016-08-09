276d1dd78469ab5a9a2e1207ae01292023ffdc99
bizui <bizuilize@gmail.com>
2016-08-09 17:28:41 -0400

    add elevation API unit tests

---
+++ test/test-apiDocs.js
@@ -0,0 +1,27 @@
+var request = require('supertest');
+var server = require('../app.js');
+
+describe('GET API docs', function() {
+  it('should render API docs', function(done) {
+    request(server)
+      .get('/api-docs')
+      .expect(200, done);
+  });
+});
+
+
+/*describe('GET contour version', function() {
+  it('should return module versions', function(done) {
+    request(server)
+      .get('/getVersions')
+      .expect(200, done);
+  });
+});*/
+
+describe('Test 404', function() {
+  it('should return a 404 error', function(done) {
+    request(server)
+      .get('/asdfsad')
+      .expect(404, done);
+  });
+});

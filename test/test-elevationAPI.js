2935fb86f47e41034a53f72e2872b86d2be0700e
bizui <bizuilize@gmail.com>
2016-08-12 11:22:53 -0400

    add unit tests src and unit

--- test/test-elevationAPI.js
+++ test/test-elevationAPI.js
@@ -2,117 +2,183 @@ var request = require('supertest');
 var server = require('../app.js');
 
 describe('Elevation API test', function() {
-  it('should return elevation data if src or unit provided', function(done) {
-    
-    request(server)
-      .get('/elevation.json?lat=38.22&long=-78.5&src=ned_1&unit=meters')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        if (err) {
-          throw err;
+
+    describe('lat/lon', function() {
+        it('should return elevation data based on lat and lon only', function(done) {
+
+            request(server)
+                .get('/elevation.json?lat=38.22&long=-78.5')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.should.have.property('elevation');
+                    done();
+                });
+        });
+
+        it('should not return elevation data if lat and lon are not provided', function(done) {
+
+            request(server)
+                .get('/elevation.json')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.should.have.property('statusCode').be.equal('400');
+                    res.body.should.have.property('statusMessage').be.equal('unable to process elevation data');
+                    done();
+                });
+        });
+
+        it('should check for invalid lat/lon input values', function(done) {
+
+            request(server)
+                .get('/elevation.json?lat=9999&long=9999')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.should.have.property('statusCode').be.equal('400');
+                    res.body.should.have.property('statusMessage').be.equal('Invalid Input - Latitude/Longitude');
+                    done();
+                });
+        });
+    });
+
+    describe('src', function() {
+        var srcVals = {
+            'ned_1': '3DEP 1 arc-second',
+            // 'ned_13': '3DEP 1/3 arc-second',            
+            'usgs': '3DEP 1/3 arc-second'
+        };
+
+        for (var key in srcVals) {
+            chkSrc(key);
         }
 
-        res.body.should.have.property('elevation').be.equal(163.24);        
-        done();
-      });
-  });
+        function chkSrc(key) {
+            it('should return elevation data if src = ' + key, function(done) {
+              var url = '/elevation.json?lat=38.22&long=-78.5&src=' + key;
 
-  it('should return elevation data if src or unit are not provided', function(done) {
-    
-    request(server)
-      .get('/elevation.json?lat=38.22&long=-78.5')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        if (err) {
-          throw err;
+                request(server)
+                    .get(url)
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end(function(err, res) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        res.body.should.have.property('data source').be.equal(srcVals[key]);
+                        done();
+                    });
+            });
         }
 
-        res.body.should.have.property('elevation').be.equal(163.24);        
-        done();
-      });
-  });
+        it('should check for invalid src values', function(done) {
 
-  it('should return JSON format', function(done) {    
+            request(server)
+                .get('/elevation.json?lat=38.33&long=-78.2&src=9999&unit=9999')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
 
-    request(server)
-      .get('/elevation.json?lat=38.22&long=-78.5&src=ned_1&unit=meters')
-      .expect('Content-Type', /json/)
-      .expect(200, done);
-  });
+                    res.body.should.have.property('statusCode').be.equal('400');
+                    res.body.should.have.property('statusMessage').be.equal('Invalid Input - Source');
+                    done();
+                });
+        });
+    });
 
-  it('should return elevation data in meters', function(done) {
-    
-    request(server)
-      .get('/elevation.json?lat=38.22&long=-78.5&src=ned_1&unit=meters')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        if (err) {
-          throw err;
+    describe('unit', function() {
+        var unitVals = {
+            'meters': 'meters'
+                // 'miles': 'miles',
+                // 'feet': 'ft'
+        };
+
+        for (var key in unitVals) {
+            chkUnit(key);
+        }       
+
+        function chkUnit(key) {
+            it('should return elevation data in ' + key, function(done) {
+              var url = '/elevation.json?lat=38.22&long=-78.5&unit=' + key;
+
+                request(server)
+                    .get(url)
+                    .expect('Content-Type', /json/)
+                    .expect(200)
+                    .end(function(err, res) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        res.body.should.have.property('unit').be.equal(unitVals[key]);
+                        done();
+                    });
+            });
         }
 
-        res.body.should.have.property('unit').be.equal('meters');        
-        done();
-      });
-  });
+        it('should check for invalid unit values', function(done) {
 
-  
+            request(server)
+                .get('/elevation.json?lat=38.33&long=-78.2&unit=9999')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
 
-  it('should check for invalid lat/lon input values', function(done) {
-    
-    request(server)
-      .get('/elevation.json?lat=9999&long=9999&src=ned_1&unit=meters')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        if (err) {
-          throw err;
-        }
+                    res.body.should.have.property('statusCode').be.equal('400');
+                    res.body.should.have.property('statusMessage').be.equal('Invalid Input - Unit');
+                    done();
+                });
+        });
 
-        res.body.should.have.property('statusCode').be.equal('400');
-        res.body.should.have.property('statusMessage').be.equal('Invalid Input - Latitude/Longitude');        
-        done();
-      });
-  });
+    });
 
-  it('should check for invalid src input values', function(done) {
-    
-    request(server)
-      .get('/elevation.json?lat=38.33&long=-78.2&src=9999&unit=9999')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        if (err) {
-          throw err;
-        }
+    describe('src and unit', function() {
 
-        res.body.should.have.property('statusCode').be.equal('400');
-        res.body.should.have.property('statusMessage').be.equal('Invalid Input - Source');        
-        done();
-      });
-  });
+        it('should return elevation data if src and unit provided', function(done) {
 
-  it('should check for invalid unit input values', function(done) {
-    
-    request(server)
-      .get('/elevation.json?lat=38.33&long=-78.2&unit=9999')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        if (err) {
-          throw err;
-        }
+            request(server)
+                .get('/elevation.json?lat=38.22&long=-78.5&src=ned_1&unit=meters')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
 
-        res.body.should.have.property('statusCode').be.equal('400');
-        res.body.should.have.property('statusMessage').be.equal('Invalid Input - Unit');        
-        done();
-      });
-  });
-  
+                    res.body.should.have.property('elevation');
+                    done();
+                });
+        });
 
-  
-  
+    });
+
+    describe('format', function() {
+        it('should return JSON format', function(done) {
+
+            request(server)
+                .get('/elevation.json?lat=38.22&long=-78.5&src=ned_1&unit=meters')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
 });
-

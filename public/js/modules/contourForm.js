8e91bdfc6a337d4bc2b01a1e05ff2658b77afed3
bizui <bizuilize@gmail.com>
2016-09-08 16:06:48 -0400

    consolidate Handlebars template

--- public/js/modules/contourForm.js
+++ public/js/modules/contourForm.js
@@ -65,7 +65,7 @@
         },
         createTemplate: function(data) {
             var fields = {};
-            var source = $('#contour-template').html();
+            var source = $('#apiForm-template').html();
             var template, fieldsetHTML;
 
             template = Handlebars.compile(source);

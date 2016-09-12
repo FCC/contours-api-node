6f11ecbb706569e5a421791cd7aa55d4db95b562
bizui <bizuilize@gmail.com>
2016-09-12 12:49:34 -0400

    update to use GeoJSON output

--- public/js/modules/profileMap.js
+++ public/js/modules/profileMap.js
@@ -20,13 +20,15 @@
             var haatMeta = '<dl class="dl-profile dl-horizontal">';
             
             haatMeta += '<dt>Average Elevation:</dt>';
-            haatMeta += '<dd>' + data.average_elevation + ' ' + data.unit + '</dd>';
+            haatMeta += '<dd>' + data.features[0].properties.average_elevation[0] + ' ' + data.features[0].properties.unit + '</dd>';
             haatMeta += '<dt>Latitude:</dt>';
-            haatMeta += '<dd>' + data.lat + '</dd>';
+            haatMeta += '<dd>' + data.features[0].properties.lat + '</dd>';
             haatMeta += '<dt>Longitude:</dt>';
-            haatMeta += '<dd>' + data.lon + '</dd>';
+            haatMeta += '<dd>' + data.features[0].properties.lon + '</dd>';
+            haatMeta += '<dt>Azimuth:</dt>';
+            haatMeta += '<dd>' + data.features[0].properties.azimuth + '</dd>';
             haatMeta += '<dt>Data Source:</dt>';
-            haatMeta += '<dd>' + data.elevation_data_source + '</dd>';
+            haatMeta += '<dd>' + data.features[0].properties.elevation_data_source + '</dd>';
             haatMeta += '</dl>';
 
             return haatMeta;

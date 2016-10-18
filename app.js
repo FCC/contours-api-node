e9ebecc4e0b1868a64ac6da40a762c9a7075d665
Laxmisekar Pendem <lpendem@nciinc.com>
2016-10-18 12:39:50 -0400

    entity API commented

--- app.js
+++ app.js
@@ -392,10 +392,10 @@ app.get('/distance.json', function(req, res){
     });   
 });
 
-app.get('/entity.json', function(req, res){
+/*app.get('/entity.json', function(req, res){
     entity.getEntity(req, res);
 });
-
+*/
 
 // **********************************************************
 // error

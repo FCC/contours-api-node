6ad8cd981dd049bb2b7a98aafe9964642d9d73ab
Laxmisekar Pendem <lpendem@nciinc.com>
2016-10-27 16:06:54 -0400

    disabled entity API for AT push

--- app.js
+++ app.js
@@ -419,9 +419,9 @@ app.get('/distance.json', function(req, res){
     });   
 });
 
-app.get('/entity.json', function(req, res){
+/*app.get('/entity.json', function(req, res){
     entity.getEntity(req, res);
-});
+});*/
 
 // **********************************************************
 // error

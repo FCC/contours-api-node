fa903a397bd87975bc640aa6606bbf801cec2a3d
Ahmad.Aburizaiza <aaburizaiza@nciinc.com>
2018-02-02 18:23:02 -0500

    testing.json message modified

--- controllers/testing.js
+++ controllers/testing.js
@@ -2,7 +2,7 @@
 var getTesting = function(req,res){
     
     res.status(200);
-    res.send('Responce successful!');
+    res.send('Responce from testing.json API is successful!');
 };
 
 module.exports.getTesting = getTesting;

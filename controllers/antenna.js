92731c4f7824ede78d7b870ce2055da4205eaedb
Ahmad.Aburizaiza <aaburizaiza@nciinc.com>
2018-02-02 11:48:10 -0500

    Resetted some commits

--- controllers/antenna.js
+++ controllers/antenna.js
@@ -1,23 +1,5 @@
-
-// **********************************************************
-'use strict';
-// **********************************************************
-
-var dotenv = require('dotenv').load();
-var NODE_ENV = process.env.NODE_ENV;
-var NODE_PORT =  process.env.PORT;
-var host =  process.env.HOST;
-var geo_host =  process.env.GEO_HOST;
-var geo_space = process.env.GEO_SPACE;
-var EFS_ELEVATION_DATASET = process.env.EFS_ELEVATION_DATASET;
-var LMS_PG = process.env.LMS_PG;
-var LMS_SCHEMA = process.env.LMS_SCHEMA;
+var LMS_SCHEMA = 'mass_media';
 var db_lms = require('./db_lms.js');
-var contours = require('./contours.js');
-var CONTEXT_PATH = process.env.CONTEXT_PATH || 'api/contours/';
-if (NODE_ENV == 'DEV' || NODE_ENV == 'LOCAL') {
-	var CONTEXT_PATH = '';
-}
 
 
 // this function returns the lng(s), lat(s) by passing application_id, facility_id, or callsign
@@ -28,7 +10,6 @@ function getAntenna(req, res, callback) {
 	var application_id = req.query.applicationId;
 	var facility_id = req.query.facilityId;
 	var callsign = req.query.callsign;
-	callsign = callsign.toUpperCase();
 	var service_type = req.query.serviceType;
 	
 	// checkQueryParams return true if parameters are valid
@@ -58,6 +39,7 @@ function getAntenna(req, res, callback) {
 			query_by_facility_id(facility_id,service_type,res);
 		}
 		else if (application_id == undefined && facility_id == undefined && callsign != undefined){
+			callsign = callsign.toUpperCase()
 			query_by_callsign(callsign,service_type,res);
 		}
 	}

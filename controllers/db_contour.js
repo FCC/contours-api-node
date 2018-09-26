916fa1ce0592eeb2cfff000dec2b29f3640702ac
sambressi <Samuel.Bressi@fcc.gov>
2018-09-26 11:37:20 -0400

    added LMS live database connection

--- controllers/db_contour.js
+++ controllers/db_contour.js
@@ -1,15 +1,7 @@
 
 var dotenv = require('dotenv').load();
-var NODE_ENV = process.env.NODE_ENV;
-var NODE_PORT =  process.env.PORT;
-var host =  process.env.HOST;
-var geo_host =  process.env.GEO_HOST;
-var geo_space = process.env.GEO_SPACE;
-var EFS_ELEVATION_DATASET = process.env.EFS_ELEVATION_DATASET;
-var LMS_PG = process.env.LMS_PG;
-var LMS_SCHEMA = process.env.LMS_SCHEMA;
+
 var CONTOURS_PG = process.env.CONTOURS_PG;
-var CONTOURS_SCHEMA = process.env.CONTOURS_SCHEMA;
 
 var promise = require('bluebird');
 var options = {
--- controllers/db_lms_live.js
+++ controllers/db_lms_live.js
@@ -1,7 +1,7 @@
 
 var dotenv = require('dotenv').load();
 
-var LMS_PG = process.env.LMS_PG;
+var LMS_LIVE_PG = process.env.LMS_LIVE_PG;
 
 var promise = require('bluebird');
 var options = {
@@ -9,14 +9,14 @@ var options = {
   promiseLib: promise
 };
 var pgp = require('pg-promise')(options);
-var db_lms = null;
+var db_lms_live = null;
 try {
-	db_lms = pgp(LMS_PG);
-	console.log('\n' + 'connected to LMS DB from db_lms.js');
+	db_lms_live = pgp(LMS_LIVE_PG);
+	console.log('\n' + 'connected to LMS LIVE DB from db_lms_live.js');
 }
 catch(e) {
-	console.log('\n' + 'connection to LMS DB failed' + e);
+	console.log('\n' + 'connection to LMS LIVE DB failed' + e);
 }
 
 // Exporting the database object for shared use:
-module.exports = db_lms;
+module.exports = db_lms_live;
